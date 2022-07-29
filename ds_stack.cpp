47ff7712f9a796ce93c4e1318ac5e4da6d0c7ed3
Kaerrick Rajotte-Carlson <kaerrickrc@yahoo.com>
2022-07-28 19:23:20 -0500

    Implemented: - is_empty() - print()

--- ds_stack.cpp
+++ ds_stack.cpp
@@ -1,44 +1,66 @@
 
 module;
 #include <vector>
+#include <iostream>
+#include <string>
 
 module ds_stack;
 
 
-template<typename T, size_t size = 10>
+template<typename T>
 class ds_stack {
 
 	std::vector<T> stack_vector;
 
 public:
 
-	//ds_stack(size_t size = 10) { stack_vector(size); }
+	ds_stack(size_t size = 10) { stack_vector(size); }
 
 	void push( T& item );
+	
 	T& pop();
+
+	bool is_empty();
+
 	void print();
 
 };
 
-template<typename T, size_t size> 
-void ds_stack<T, size>::push( T& item ){
+
+// Primary Methods
+
+template<typename T> 
+void ds_stack<T>::push( T& item ){
 
 	stack_vector.push_back( item );
 
 }
 
 
-template<typename T, size_t size>
-T& ds_stack<T, size>::pop() {
+template<typename T>
+T& ds_stack<T>::pop() {
 	
 	T item = stack_vector.back();
 	stack_vector.pop_back();
 	return item;
+
 }
 
 
-template<typename T, size_t size>
-void ds_stack<T, size>::print() {
+template<typename T>
+bool ds_stack<T>::is_empty() {
+	return stack_vector.is_empty();
+}
+
+
+// Debug Methods 
+
+template<typename T>
+void ds_stack<T>::print() {
+
+	for( int i = 0; i < stack_vector.size(); i++ ){
+		std::cout << std::to_string(i) << ": " << std::to_string(stack_vector[i]) << std::endl;
+	}
 
 }
 
